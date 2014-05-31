5f69e19eeb52241020dc8ed3a3182a16adb28849
Franklin Wei <frankhwei536@gmail.com>
2014-05-31 18:12:45 -0400

    Enhanced performance by making exec_table global to eliminated re-init each exec

--- src/exec.c
+++ src/exec.c
@@ -120,38 +120,40 @@ static void exec_1A(alpha_ctx* ctx, byte op1, byte op2, byte op3)
   ctx->regs[op3]-=4;
   ctx->regs[op2]=readWord(ctx, ctx->regs[op3]);
 }
+
+static void (*exec_table[256])(alpha_ctx*, byte, byte, byte) = {
+  &exec_00,
+  &exec_01,
+  &exec_02,
+  &exec_03,
+  &exec_04,
+  &exec_05,
+  &exec_06,
+  &exec_07,
+  &exec_08,
+  &exec_09,
+  &exec_0A,
+  &exec_0B,
+  &exec_0C,
+  &exec_0D,
+  &exec_0E,
+  &exec_0F,
+  &exec_10,
+  &exec_11,
+  &exec_12,
+  &exec_13,
+  &exec_14,
+  &exec_15,
+  &exec_16,
+  &exec_17,
+  &exec_18,
+  &exec_19,
+  &exec_1A,
+#include <null-table.h>
+};
+
 void exec_opcode(alpha_ctx* ctx, byte opcode, byte op1, byte op2, byte op3)
 {
-  void (*exec_table[256])(alpha_ctx*, byte, byte, byte) = {
-    &exec_00,
-    &exec_01,
-    &exec_02,
-    &exec_03,
-    &exec_04,
-    &exec_05,
-    &exec_06,
-    &exec_07,
-    &exec_08,
-    &exec_09,
-    &exec_0A,
-    &exec_0B,
-    &exec_0C,
-    &exec_0D,
-    &exec_0E,
-    &exec_0F,
-    &exec_10,
-    &exec_11,
-    &exec_12,
-    &exec_13,
-    &exec_14,
-    &exec_15,
-    &exec_16,
-    &exec_17,
-    &exec_18,
-    &exec_19,
-    &exec_1A,
-#include <null-table.h>
-  };
   if(exec_table[opcode])
     {
       exec_table[opcode](ctx, op1, op2, op3);
